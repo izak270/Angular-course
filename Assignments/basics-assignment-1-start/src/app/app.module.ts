3151df14f6001e203abfc8b0b5328868dc427f0e
izak270 <izak270@gmail.com>
2020-03-13 19:29:22 +0200

    Assaingnment 2

--- Assignments/basics-assignment-1-start/src/app/app.module.ts
+++ Assignments/basics-assignment-1-start/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { UsernameComponent } from './username/username.component';
 
 @NgModule({
-  declarations: [AppComponent, UsernameComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
