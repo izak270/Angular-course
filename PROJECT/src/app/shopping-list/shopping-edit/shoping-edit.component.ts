91350709a719e2bb817d0d82c558be3414bb9335
izak270 <izak270@gmail.com>
2020-03-15 16:55:00 +0200

    Set course project

---
+++ PROJECT/src/app/shopping-list/shopping-edit/shoping-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shoping-edit.component.html',
+  styleUrls: ['./shoping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
